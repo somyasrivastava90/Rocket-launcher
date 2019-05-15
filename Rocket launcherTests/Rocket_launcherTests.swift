527a9fb7189a4861a05f3c47609e715973d52e80
Somya <somya.bhatnagar@mobileden.com.au>
2019-05-15 13:14:43 +1000

    Initial Commit

---
+++ Rocket launcherTests/Rocket_launcherTests.swift
@@ -0,0 +1,33 @@
+//
+//  Rocket_launcherTests.swift
+//  Rocket launcherTests
+//
+//  Created by Somya on 15/5/19.
+//
+
+import XCTest
+@testable import Rocket_launcher
+
+class Rocket_launcherTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
